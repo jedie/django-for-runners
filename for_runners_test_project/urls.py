2d2c329656a7987278e00b9d1d36b50aaee8138e
JensDiemer <git@jensdiemer.de>
2018-06-02 09:32:55 +0200

    redirect to /admin/for_runners/gpxmodel/

--- for_runners_test_project/urls.py
+++ for_runners_test_project/urls.py
@@ -1,20 +1,20 @@
 # coding: utf-8
 
-
-
-
-from django.conf.urls import include, url, static
+from django.conf.urls import include, static, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.views.generic import RedirectView
 
+# https://github.com/jedie/django-for-runners
 from for_runners_test_project import settings
-
 
 admin.autodiscover()
 
-
 urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
+
+    # until there is not real CMS pages: redirect to the interesting admin page:
+    url(r"^$", RedirectView.as_view(url="/admin/for_runners/gpxmodel/")),
     url(r'^', include('cms.urls')),
 )
 
