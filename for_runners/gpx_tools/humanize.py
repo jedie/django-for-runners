3f9020ac703420d23b3f0889e038f733c677bc6d
JensDiemer <git@jensdiemer.de>
2018-06-12 22:53:58 +0200

    Display hours, too + DocTests

--- for_runners/gpx_tools/humanize.py
+++ for_runners/gpx_tools/humanize.py
@@ -8,6 +8,29 @@ import math
 
 
 def human_seconds(duration):
+    """
+    >>> human_seconds(1)
+    '0:01'
+    >>> human_seconds(61)
+    '1:01'
+    >>> human_seconds(60*60+1)
+    '1:00:01'
+    """
+    parts = []
+
+    hours = None
+    if duration>1*60*60:
+        hours = math.floor(duration/60/60)
+        duration -= hours*60*60
+        parts.append("%i" % hours)
+
     minutes = math.floor(duration/60)
+    if hours is None:
+        parts.append("%i" % minutes)
+    else:
+        parts.append("%02i" % minutes)
+
     seconds = math.floor(duration - (minutes*60))
-    return "%i:%02i" % (minutes, seconds)
+    parts.append("%02i" % seconds)
+
+    return ":".join(parts)
