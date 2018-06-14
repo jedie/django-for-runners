f5f1cd2808a7192d6229cb0c07b2e689fbdc24f6
JensDiemer <git@jensdiemer.de>
2018-06-15 00:04:05 +0200

    release as v0.1.0

--- setup.py
+++ setup.py
@@ -228,9 +228,7 @@ if "publish" in sys.argv:
 
 # https://pypi.org/classifiers/
 classifiers = """
-Development Status :: 1 - Planning
-#Development Status :: 2 - Pre-Alpha
-#Development Status :: 3 - Alpha
+Development Status :: 3 - Alpha
 #Development Status :: 4 - Beta
 #Development Status :: 5 - Production/Stable
 #Development Status :: 6 - Mature
