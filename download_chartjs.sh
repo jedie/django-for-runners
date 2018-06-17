52aed7b3f6ec3912b1b6e9091357eba228438b15
JensDiemer <git@jensdiemer.de>
2018-06-17 17:38:48 +0200

    Add static files (Charts.JS + Leaflet)

---
+++ download_chartjs.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#
+# Update Leaflet
+#
+
+set -ex
+
+cd for_runners/static/chartjs
+
+#
+# download link from:
+# https://leafletjs.com/download.html
+#
+wget https://github.com/chartjs/Chart.js/releases/download/v2.7.2/Chart.bundle.min.js
