b31b444127618fe0eed688e477a0e174f31a5209
JensDiemer <git@jensdiemer.de>
2018-07-04 23:43:31 +0200

    +DocString

--- for_runners/apps.py
+++ for_runners/apps.py
@@ -1,7 +1,13 @@
+"""
+    https://docs.djangoproject.com/en/2.0/ref/applications/#configuring-applications-ref
 
-# https://docs.djangoproject.com/en/2.0/ref/applications/#configuring-applications-ref
+    created 04.07.2018 by Jens Diemer <opensource@jensdiemer.de>
+    :copyleft: 2018 by the django-for-runners team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
 
 from django.apps import AppConfig
+
 
 class ForRunnersConfig(AppConfig):
     name = 'for_runners'
