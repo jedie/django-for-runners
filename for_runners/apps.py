f7e509b4b9d7bc618d92c10792efb17a0b111df8
JensDiemer <git@jensdiemer.de>
2018-07-04 22:49:35 +0200

    add a AppConfig

---
+++ for_runners/apps.py
@@ -0,0 +1,8 @@
+
+# https://docs.djangoproject.com/en/2.0/ref/applications/#configuring-applications-ref
+
+from django.apps import AppConfig
+
+class ForRunnersConfig(AppConfig):
+    name = 'for_runners'
+    verbose_name = "ForRunners"
