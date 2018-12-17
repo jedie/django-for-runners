2954557721d218f193f36c8446c1ff280ce854b4
JensDiemer <git@jensdiemer.de>
2018-12-17 15:53:49 +0100

    remove admin action "export as json"

--- for_runners/admin/__init__.py
+++ for_runners/admin/__init__.py
@@ -3,13 +3,8 @@
     :copyleft: 2018 by the django-for-runners team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-from django.contrib import admin
 
 from for_runners.admin.discipline import DisciplineModelAdmin
 from for_runners.admin.distance import DistanceModelAdmin
 from for_runners.admin.event import EventLinkModelAdmin, EventModelAdmin, ParticipationModelAdmin
 from for_runners.admin.gpx import GpxModelAdmin
-from for_runners.admin.utils import export_as_json
-
-# Make export actions available site-wide
-admin.site.add_action(export_as_json, 'export_selected_as_json')
--- for_runners/admin/utils.py
+++ for_runners/admin/utils.py
@@ -3,16 +3,12 @@
     :copyleft: 2018 by the django-for-runners team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
-from django.contrib import admin
-from django.contrib.contenttypes.models import ContentType
-from django.core import serializers
-from django.http import HttpResponse, HttpResponseRedirect
+
 from django.views import generic
 from django.views.generic import TemplateView
 
 
 class ChangelistViewMixin:
-
     def dispatch(self, request, change_list, *args, **kwargs):
         self.change_list = change_list
         return super().dispatch(request, *args, **kwargs)
@@ -22,6 +18,7 @@ class BaseChangelistView(ChangelistViewMixin, TemplateView):
     """
     Baseclass for chnagelist views without forms.
     """
+
     pass
 
 
@@ -29,30 +26,9 @@ class BaseFormChangelistView(ChangelistViewMixin, generic.FormView):
     """
     Baseclass for changelist views with forms.
     """
+
     form_class = None
 
     def form_valid(self, form):
         # Don't redirect, if form is valid ;)
         return self.render_to_response(self.get_context_data(form=form))
-
-
-# def export_as_json(modeladmin, request, queryset):
-#     response = HttpResponse(content_type="application/json")
-#     serializers.serialize("json", queryset, stream=response)
-#     return response
-#
-#
-# def export_selected_objects(modeladmin, request, queryset):
-#     selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
-#     ct = ContentType.objects.get_for_model(queryset.model)
-#     return HttpResponseRedirect("/export/?ct=%s&ids=%s" % (ct.pk, ",".join(selected)))
-
-def export_as_json(modeladmin, request, queryset):
-    """
-    from:
-    http://docs.djangoproject.com/en/dev/ref/contrib/admin/actions/#actions-that-provide-intermediate-pages
-    """
-    # response = HttpResponse(content_type="text/javascript")
-    response = HttpResponse(content_type="application/json")
-    serializers.serialize("json", queryset, stream=response, indent=4)
-    return response
