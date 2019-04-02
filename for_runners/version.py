054755b588d84f4bb57ad84ef64862ee94b4019f
JensDiemer <git@jensdiemer.de>
2019-04-02 19:13:16 +0200

    release as v0.9.0

--- for_runners/version.py
+++ for_runners/version.py
@@ -1,7 +1,7 @@
 """
     created 30.05.2018 by Jens Diemer <opensource@jensdiemer.de>
-    :copyleft: 2018 by the django-for-runners team, see AUTHORS for more details.
+    :copyleft: 2018-2019 by the django-for-runners team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-__version__ = "0.8.1"
+__version__ = "0.9.0"
