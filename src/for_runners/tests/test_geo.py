2df1deef6f6772c7e7b685f2077279e81c721dee
JensDiemer <git@jensdiemer.de>
2020-11-14 20:41:26 +0100

    mock geo() request against "nominatim.openstreetmap.org/reverse"

--- src/for_runners/tests/test_geo.py
+++ src/for_runners/tests/test_geo.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-# https://github.com/jedie/django-for-runners
+import requests_mock
+
 from for_runners.geo import reverse_geo
 
 
@@ -11,6 +12,49 @@ class GeoTests(TestCase):
         lat = "51.43788929097354412078857421875"
         lon = "6.617012657225131988525390625"
 
-        address = reverse_geo(lat, lon)
+        osm_json_response = {
+            'place_id': 273953846,
+            'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright',
+            'osm_type': 'way',
+            'osm_id': 764409683,
+            'lat': '51.438802550000005',
+            'lon': '6.616602053045958',
+            'display_name': (
+                        '148, Filder Straße, Vinn,'
+                        ' Moers, Kreis Wesel,'
+                        ' North Rhine-Westphalia,'
+                        ' 47447, Germany'
+            ),
+            'address': {
+                'house_number': '148',
+                'road': 'Filder Straße',
+                        'hamlet': 'Vinn',
+                        'suburb': 'Moers',
+                        'city': 'Moers',
+                        'county': 'Kreis Wesel',
+                        'state': 'North Rhine-Westphalia',
+                        'postcode': '47447',
+                        'country': 'Germany',
+                        'country_code': 'de'},
+            'boundingbox': [
+                '51.4387285',
+                '51.4388694',
+                '6.6162302',
+                '6.6169724']}
+
+        with requests_mock.mock() as m:
+            m.get(
+                ('https://nominatim.openstreetmap.org/reverse'
+                 '?lat=51.437889290973544'
+                 '&lon=6.617012657225132'
+                 '&format=json'
+                 '&addressdetails=1'),
+                headers={'content-type': 'application/json'},
+                json=osm_json_response
+            )
+            address = reverse_geo(lat, lon)
+
         assert address.short == 'Moers'
-        assert address.full.endswith('Moers, Kreis Wesel, Nordrhein-Westfalen, 47447, Deutschland')
+        assert address.full == (
+            '148, Filder Straße, Vinn, Moers, Kreis Wesel, North Rhine-Westphalia, 47447, Germany'
+        )
