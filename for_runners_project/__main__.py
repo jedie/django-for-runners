ea8ea9718f6404434091b5cd452a2f34a361cfee
JensDiemer <git@jensdiemer.de>
2023-04-02 17:57:52 +0200

    -prod_settings

--- for_runners_project/__main__.py
+++ for_runners_project/__main__.py
@@ -19,7 +19,6 @@ def main():
         config=ManageConfig(
             module=for_runners,
             project_root_path=Path(for_runners.__file__).parent.parent,
-            prod_settings='for_runners_project.settings.prod',
             local_settings='for_runners_project.settings.local',
             test_settings='for_runners_project.settings.tests',
         )
