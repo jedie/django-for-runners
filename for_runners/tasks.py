0ee151d7c52c6313cb8c6b3ba8b7d4e60caf08d3
JensDiemer <git@jensdiemer.de>
2024-07-31 16:14:08 +0200

    Replace "autotask" with "autocron"

--- for_runners/tasks.py
+++ for_runners/tasks.py
@@ -4,38 +4,40 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-# import io
-# import logging
-#
-# from autotask.tasks import delayed_task
-# from for_runners.gpx_tools.garmin2gpxpy import garmin2gpxpy
-#
-#
-# log = logging.getLogger(__name__)
-#
-#
-# @delayed_task()
-# def generate_gpx_map_task(object_id):
-#     """
-#     Delayed task to generate the map from GPX track
-#     """
-#     log.debug("Generate GPX Map for ID: %r", object_id)
-#
-#     from for_runners.models import GpxModel # import here, because of import-loop
-#     gpx_instance = GpxModel.objects.get(pk=object_id)
-#     log.info("Generate GPX Map for: %s" % gpx_instance)
-#
-#     content = gpx_instance.gpx
-#     gpxpy_instance = garmin2gpxpy(content)
-#
-#     image, plt = generate_map(gpxpy_instance)
-#
-#     temp = io.BytesIO()
-#     plt.savefig(temp, bbox_inches="tight")
-#
-#     filename = "%s.png" % gpx_instance.get_short_slug()
-#
-#     # Save gpx map file to model instance:
-#     gpx_instance.map_image.save(filename, temp)
-#
-#     log.info("GPX data saved to %s, ok." % gpx_instance)
+import io
+import logging
+
+import autocron
+
+from for_runners.gpx_tools.garmin2gpxpy import garmin2gpxpy
+
+
+log = logging.getLogger(__name__)
+
+
+@autocron.delay
+def generate_gpx_map_task(object_id):
+    """
+    Delayed task to generate the map from GPX track
+    """
+    log.debug("Generate GPX Map for ID: %r", object_id)
+
+    from for_runners.models import GpxModel  # import here, because of import-loop
+
+    gpx_instance = GpxModel.objects.get(pk=object_id)
+    log.info(f"Generate GPX Map for: {gpx_instance}")
+
+    content = gpx_instance.gpx
+    gpxpy_instance = garmin2gpxpy(content)
+
+    image, plt = generate_map(gpxpy_instance)
+
+    temp = io.BytesIO()
+    plt.savefig(temp, bbox_inches="tight")
+
+    filename = f"{gpx_instance.get_short_slug()}.png"
+
+    # Save gpx map file to model instance:
+    gpx_instance.map_image.save(filename, temp)
+
+    log.info(f"GPX data saved to {gpx_instance}, ok.")
